08b8bd7cf27f8aded2a7af807048fc64ecdecbbe
matthew41305 <matthew41305@gmail.com>
2024-12-01 23:10:47 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("What worked well for me was learning to pick up the book more and actually study it and whenever I got confused I'd go back to the slides.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("I wouldn't really change much, the assignments are great, I would just like some reading to be done in class then talked about with one another");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("Yes, I learned what I expected from this course, more that I thought I would to be honest.");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("I haven't encountered anything javascript related before this class, although it was similar to C# I never messed with Javascript and always thought it was the same as Java.");
